6c505bb9886455b8c1b66797e7cc53484378c13c
rotom407 <gqsxstc@163.com>
2020-01-09 17:13:55 +0800

    storage

--- MarvisConsole/RendererWrapper.cs
+++ MarvisConsole/RendererWrapper.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tao.OpenGl;
-using Tao.FreeGlut;
-using Tao.Platform.Windows;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Platform.Windows;
 
 namespace MarvisConsole {
     public class Particle {
